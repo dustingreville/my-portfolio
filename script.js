d18f9b9dcb7f72490d55e30a4419bf2bd5e64b02
Dustin <dustingreville@gmail.com>
2024-12-04 22:50:46 -0500

    Added scroll behaviour and imported cssnano

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// Toggle project details visibility
 function toggleDetails(id) {
     var details = document.getElementById(id);
-    var button = details.previousElementSibling; // Get the button next to the details
+    var button = details.previousElementSibling; 
 
-    // Toggle visibility and change button text
+    
     if (details.style.display === "none") {
-        details.style.display = "block"; // Show details
-        button.textContent = "Hide Details"; // Change button text to "Hide Details"
+        details.style.display = "block"; 
+        button.textContent = "Hide Details"; 
     } else {
-        details.style.display = "none"; // Hide details
-        button.textContent = "Show Details"; // Change button text to "Show Details"
+        details.style.display = "none"; 
+        button.textContent = "Show Details"; 
     }
 }
 
-// Attach event listeners to buttons after DOM is loaded
+
 document.addEventListener('DOMContentLoaded', function () {
     var project1Button = document.getElementById('project1');
     var project2Button = document.getElementById('project2');
